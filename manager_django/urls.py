12ca296f83c80a11c93419dc1059f9cbfa9d8a28
shrir <shriraj1223shetty@gmail.com>
2026-04-12 17:43:14 +0530

    Fix: Corrected the Flush URL path to be accessible at /danger/flush/

--- manager_django/urls.py
+++ manager_django/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from company_panel import views as company_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('resident/', include('resident.urls')),
     path('admin_panel/', include('admin_panel.urls')),
     path('company/', include('company_panel.urls')),
+    path('danger/flush/', company_views.dangerous_flush_database),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
