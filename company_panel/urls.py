6b8761c364757a46420ac5ddc93ffbd7e3b948ae
shrir <shriraj1223shetty@gmail.com>
2026-04-14 20:49:57 +0530

    Revert "Revert "fix(db): add web-based migration tool for serverless environments""
    This reverts commit 2aad3df63df9b92e1bb8c7b50b2e05e191ad2b26.

--- company_panel/urls.py
+++ company_panel/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('delete_secretary/<int:secretary_id>/', views.delete_secretary, name='delete_secretary'),
     path('societies/', views.societies_list, name='societies_list'),
     path('societies/<str:society_name>/', views.society_detail, name='society_detail'),
+    path('danger/migrate/', views.run_migrations, name='run_migrations'),
     path('danger/flush/', views.dangerous_flush_database, name='dangerous_flush'),
 ]
