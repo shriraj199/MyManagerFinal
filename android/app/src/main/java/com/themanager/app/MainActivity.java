4b6d4cb307a5712e6481b6af661b18f4b1d8738d
shrir <shriraj1223shetty@gmail.com>
2026-04-19 20:17:38 +0530

    Enabled PDF downloads in Android APK via DownloadListener

--- android/app/src/main/java/com/themanager/app/MainActivity.java
+++ android/app/src/main/java/com/themanager/app/MainActivity.java
@@ -1,5 +1,31 @@
 package com.themanager.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.DownloadListener;
+import android.content.Intent;
+import android.net.Uri;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            webView.setDownloadListener(new DownloadListener() {
+                @Override
+                public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+            });
+        }
+    }
+}
